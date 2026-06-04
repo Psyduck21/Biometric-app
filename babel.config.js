aa83e8471d68c755b4130b806666d82ffa9dfef3
Psyduck21 <akshat.prj@gmail.com>
2026-06-04 20:03:58 +0530

    chore: migrate from react-native-worklets-core to react-native-worklets plugin

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ export default function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['react-native-worklets-core/plugin'],
+      ['react-native-worklets/plugin'],
     ],
   };
 };
